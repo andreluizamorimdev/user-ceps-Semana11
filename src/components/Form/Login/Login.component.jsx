c7047292004566e77fe5ad34876440083a76849b
andreluizamorimdev <andreluizamorim.dev@gmail.com>
2023-07-12 19:05:13 -0300

    Criando estilos do componente e pagina login.

--- src/components/Form/Login/Login.component.jsx
+++ src/components/Form/Login/Login.component.jsx
@@ -1,27 +1,56 @@
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import * as Styled from './Login.style';
 
 const FormLoginComponent = () => {
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+    })
     const navigate = useNavigate();
+
+    const isDisabled = () => {
+        return !data.email || !data.password || !data.email.includes('@') || data.password.length < 8;
+    }
+
+    const handleInput = (event) => {
+        event.preventDefault();
+        const {value, id} = event.target;
+
+        setData({...data, [id]: value});
+    }
 
     const redirectToHome = () => {
         navigate('/home');
     }
 
     return (
-        <form onSubmit={redirectToHome}>
-            <legend>Login</legend>
-            <div className="input-group">
-                <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" placeholder="Digite seu email" />
-            </div>
+        <Styled.Form onSubmit={redirectToHome}>
+            <Styled.Header>
+                <Styled.Title>Login</Styled.Title>
+                <Styled.SubTitle>Para acessar o sistema digite seu email e sua senha.</Styled.SubTitle>
+            </Styled.Header>
 
-            <div className="input-group">
-                <label htmlFor="password">Senha</label>
-                <input type="password" id="password" placeholder="Digite sua senha" />
-            </div>
+            <Styled.InputGroup>
+                <div className="input-group">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" onInput={handleInput} placeholder="Digite seu email" />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" id="password" onInput={handleInput} placeholder="Digite sua senha" />
+                </div>
+            </Styled.InputGroup>
             
-            <button type="submit">Logar</button>
-        </form>
+            <Styled.Button type="submit" disabled={isDisabled()}>Entrar</Styled.Button>
+
+            <Styled.Action className="Action">
+                <Styled.EsqueciSenha href="">Esqueci minha senha</Styled.EsqueciSenha>
+                <Styled.Button type="button" $outlined={true} >Criar conta</Styled.Button>
+            </Styled.Action>
+
+        </Styled.Form>
     );
 }
  
--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,10 +1,10 @@
 import FormLoginComponent from "../../components/Form/Login/Login.component";
-
+import * as Styled from './Login.style';
 const LoginPage = () => {
     return (
-        <>
+        <Styled.Login>
             <FormLoginComponent />
-        </>
+        </Styled.Login>
     );
 }
  
